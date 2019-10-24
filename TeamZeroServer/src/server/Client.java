1223fae9960dc445fa8a9be07545d25673f53609
Iris Esener <iris.esener@kcl.ac.uk>
2019-10-24 19:44:58 +0100

    Server initial commit

---
+++ TeamZeroServer/src/server/Client.java
@@ -0,0 +1,34 @@
+package server;
+/**
+ * 
+ * Encapsulates a single chat Client
+ *
+ */
+public class Client {
+	
+	private String username;
+	private String email;
+	private String id;
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+
+}
---
+++ TeamZeroServer/src/server/ServerMain.java
@@ -0,0 +1,12 @@
+package server;
+/**
+ * Main Server class and system entry point.
+ */
+public class ServerMain {
+	
+
+	public static void main(String[] args) {
+		
+	}
+
+}
