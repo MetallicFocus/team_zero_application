1dcca37bb507d8449a503d49b0abcdef25229a82
Tiberiu Iustin Zulean <tiberiu.zulean@kcl.ac.uk>
2019-10-25 16:09:34 +0100

    Added forms onto the registration page

--- Android/app/src/main/java/teamzero/chat/mobile/Registration.java
+++ Android/app/src/main/java/teamzero/chat/mobile/Registration.java
@@ -5,6 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 
 public class Registration extends AppCompatActivity {
 
+    /* TODO:
+        1) Method that checks if both e-mails are the same AND in a correct format
+        2) Method that checks if both passwords are the same AND strong enough
+        3) Method that passes the information to the server to store in database
+            3') Give the user a message stating that he should check his e-mail to confirm the account
+                and go back to the login screen
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
