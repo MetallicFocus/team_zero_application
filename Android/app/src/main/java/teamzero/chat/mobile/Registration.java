3684cf2a6f203ded9525ec3ccf82f1688c19b6e6
Tiberiu Iustin Zulean <tiberiu.zulean@kcl.ac.uk>
2019-10-29 16:14:13 +0000

    Added username validation and input sanitization against SQL injection

--- Android/app/src/main/java/teamzero/chat/mobile/Registration.java
+++ Android/app/src/main/java/teamzero/chat/mobile/Registration.java
@@ -1,20 +1,25 @@
 package teamzero.chat.mobile;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.PatternSyntaxException;
 
 public class Registration extends AppCompatActivity {
 
     /* TODO:
-        1) Method that checks if both e-mails are the same AND in a correct format
-        2) Method that checks if both passwords are the same AND strong enough
-        3) Method that passes the information to the server to store in database
-            3') Give the user a message stating that he should check his e-mail to confirm the account
-                and go back to the login screen
+        1) Methods to check if username is valid and available
+        2) Method that checks if both e-mails are the same AND in a correct format
+        3) Method that checks if both passwords are the same AND strong enough
         4) Protect against SQL Injection
-        5) Method to check if username is available
+        5) Method that passes the information to the server to store in database
+            5') Give the user a message stating that he should check his e-mail to confirm the account
+                and go back to the login screen
+
      */
 
     @Override
@@ -39,24 +44,38 @@ public class Registration extends AppCompatActivity {
         System.out.println("username = " + username + "\n pwd = " + password + "\n conPwd = " + confirmPassword);
         System.out.println("email = " + email + "\n confirmEmail = " + confirmEmail);
 
-        // Check if username is valid and filter it for SQL injection
-        validateUsername(username);
+        if(validateUsername(username)) {
+            // Check if username is available to register
+            //checkUsernameAvailability(username);
 
-        // Check if username is available to register (if and only if is valid first)
-        checkUsernameAvailability(username);
+            // Check if passwords are valid and if both are the same
+            //validatePasswords(password, confirmPassword);
 
-        // Check if passwords are valid, filter them for SQL injection and check if they are the same
-        validatePasswords(password, confirmPassword);
-
-        // Check if e-mails are valid, filter them for SQL injection and check if they are the same
-        validateEmails(email, confirmEmail);
+            // Check if e-mails are valid and if both are the same
+            //validateEmails(email, confirmEmail);
+        }
+        else Toast.makeText(getApplicationContext(), "Username is invalid!\nPlease try another username!", Toast.LENGTH_SHORT).show();
     }
 
-    public void validateUsername(String username) {
+    public boolean validateUsername(String username) {
         /* Check if:
-            1) It does not contain illegal characters
-            2) It does not begin with numbers
+            1) The field is not empty
+            2) It does not contain illegal characters (anything except '-', '.' and '_')
+            3) It does not start with numbers
+            4) It is between 3 and 30 characters
          */
+
+        try {
+            boolean valid = (username != null) && username.matches("\\b[a-zA-Z][a-zA-Z0-9\\-._]{3,30}\\b");
+
+            if (!valid)
+                return false;
+
+        } catch (PatternSyntaxException ex) {
+            // Invalid regex
+            ex.printStackTrace();
+        }
+        return true;
     }
 
     public void checkUsernameAvailability(String username) {
@@ -69,7 +88,12 @@ public class Registration extends AppCompatActivity {
             2) Password contains at least 1 number
             3) Password contains at least 1 special character
             4) Password is between 8 and 12 characters
+            5) They are the same
          */
+
+        // We check 1-4 only for one password (no need for both)
+
+        // Check if passwords are the same
 
     }
 
@@ -77,13 +101,35 @@ public class Registration extends AppCompatActivity {
         /* Check if:
             1) E-mail contains '@' character
             2) E-mail contains at least a dot ('.')
+           Make this method RFC822 compliant?
+           http://www.ex-parrot.com/~pdw/Mail-RFC822-Address/Mail-RFC822-Address.html
          */
 
     }
 
-    public void filterForSQLInjection(String text) {
-        /* Method that checks for SQL injection in text */
+    public String filterForSQLInjection(String text) {
+        /* Method that filters for SQL injection in text by escaping characters */
+        String newText = null;
+        if (text != null && text.length() > 0) {
+            text = text.replace("\\", "\\\\");
+            text = text.replace("'", "\\'");
+            text = text.replace("\0", "\\0");
+            text = text.replace("\n", "\\n");
+            text = text.replace("\r", "\\r");
+            text = text.replace("\"", "\\\"");
+            text = text.replace("\\x1a", "\\Z");
+            newText = text;
+        }
+        return newText;
+    }
 
+    public void sendDataToDB(String username, String password, String email) {
+        /* Method that delivers information to store in database
+        *
+        * To Think: Use PreparedStatement to make SQL Injection impossible by placing the data
+        * directly inside the database (not affecting the INSERT statement in any way)
+        *
+        * */
     }
 
 }
