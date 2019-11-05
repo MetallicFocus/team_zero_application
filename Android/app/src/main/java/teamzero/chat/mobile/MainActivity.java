9d4e376b711e6ee4d341216f025cf7c6f1848448
Tiberiu Iustin Zulean <tiberiu.zulean@kcl.ac.uk>
2019-11-05 22:55:41 +0000

    Implemented web sockets and tested connection through a demo URI

--- Android/app/src/main/java/teamzero/chat/mobile/MainActivity.java
+++ Android/app/src/main/java/teamzero/chat/mobile/MainActivity.java
@@ -1,22 +1,46 @@
 package teamzero.chat.mobile;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
 
     Button loginButton;
     Button registerButton;
     EditText usernameInput;
+    WebSocketClient mWebSocketClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        connectWebSocket();
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        mWebSocketClient.send("Testing connection with server.");
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        mWebSocketClient.close();
 
         loginButton = (Button) findViewById(R.id.login);
         registerButton = (Button) findViewById(R.id.register);
@@ -41,6 +65,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         // End of testing zone.
+    }
+
+    private void connectWebSocket() {
+
+        URI uri;
+        try {
+            // Used the following URI for testing purposes only
+            // TODO: Change URI to Heroku server
+            uri = new URI("ws://echo.websocket.org:80/");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        mWebSocketClient = new WebSocketClient(uri) {
+
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                Log.i("Websocket", "Opened");
+            }
+
+            @Override
+            public void onMessage(String message) {
+                Log.i("Websocket", "Received message = " + message);
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+                Log.i("Websocket", "Closed " + s);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
+            }
+        };
+
+        mWebSocketClient.connect();
+
     }
 
 }
