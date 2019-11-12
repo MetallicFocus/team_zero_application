e03301b64dffd4d53ef98fad73d85ae2128f767e
Zhuoling Li <zhuoling.li@kcl.ac.uk>
2019-11-12 02:54:50 +0000

    Create components for frontend so that they can be reused.

--- JavaWeb/src/main/java/teamzero/javaweb/controller/ChatController.java
+++ JavaWeb/src/main/java/teamzero/javaweb/controller/ChatController.java
@@ -1,6 +1,7 @@
 package teamzero.javaweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +11,11 @@ public class ChatController {
     @RequestMapping(value = "")
     public String initialize() {
         return "";
+    }
+
+    //test
+    @GetMapping(value = "/main")
+    public String test() {
+        return "/main";
     }
 }
